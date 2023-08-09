aa4ba35ad66c775f5501844459bd7b98c566ecea
oguzhansakalli <osakalli024@gmail.com>
2023-08-09 20:07:50 +0300

    application release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todoText) => {
+    const newTodo = { text: todoText, completed: false };
+    setTodos([...todos, newTodo]);
+  };
+
+  const onDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const onToggleComplete = (index) => {
+    const updatedTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+  // Other functions for marking tasks done, deleting tasks, etc.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">Todo List</div>
+    <div className="tasks-group">
+      <TodoList
+        todos={todos}
+        onDeleteTodo={onDeleteTodo}
+        onToggleComplete={onToggleComplete}
+      />
+      <AddTodo addTodo={addTodo} />
     </div>
+  </div>
   );
+
 }
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./AddTodo.css";
+
+function AddTodo({ addTodo }) {
+  const [showInput, setShowInput] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputToggle = () => {
+    setShowInput(!showInput);
+    setNewTask("");
+  };
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleInputSubmit = (event) => {
+    if (event.key === "Enter" && newTask.trim() !== "") {
+      addTodo(newTask);
+      setNewTask("");
+      setShowInput(false); // Hide the input after adding a task
+    }
+  };
+
+  return (
+    <div className="add-task">
+      {!showInput ? (
+        <button className="add-button" onClick={handleInputToggle}>
+          <img
+            src="images/new-task.png"
+            alt="Add New Task"
+            style={{ width: "60px", height: "60px" }}
+          />
+        </button>
+      ) : (
+        <div>
+          <img
+            src="images/quest-icon.png"
+            alt="Quest"
+            className="icon"
+            style={{ width: "50px", height: "50px" }}
+          />
+          <input
+            type="text"
+            className="task-input"
+            placeholder="Enter a new task"
+            value={newTask}
+            onChange={handleInputChange}
+            onKeyPress={handleInputSubmit}
+            autoFocus
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AddTodo;
